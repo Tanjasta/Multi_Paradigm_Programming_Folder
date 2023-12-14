65bff9b2757351bdf44e508044d69d7bf3be9436
Tatjana <g00425678@atu.ie>
2023-12-14 18:54:49 +0100

    readme proced

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Multi_Paradigm_Programming_Folder
 Multi_Paradigm_Programming_Folder
+Description
+This Python program simulates a basic shop operation. It processes orders by reading stock and customer data from CSV files. It also features a live mode for real-time order processing.
+Requirements
+* Python 3.x
+* CSV files for stock (stock.csv) and customers (customer.csv, customertest.csv, customertest2.csv)
+Usage
+* 		Prepare CSV Files: Ensure stock.csv and customer CSV files are in the same directory as the script.
+* 		Run the Script: Execute the script using Python.
+* 		Live Mode: In live mode, input product names and quantities as prompted. Type 'exit' to end live mode.
+Functions
+create_and_stock_shop(filename='stock.csv')
+Reads the stock.csv file to initialise the shop's inventory and cash.
+* filename: Path to the stock CSV file.
+read_customer(filename)
+Reads a customer's data from a CSV file.
+* filename: Path to the customer's CSV file.
+print_product(product)
+Prints details of a single product.
+* product: A dictionary containing product details.
+print_customer(customer)
+Prints customer details, including their name, cash, and product list.
+* customer: A dictionary containing customer information.
+process_order(shop, customer)
+Processes an order, updating shop inventory and cash based on the customer's order.
+* shop: A dictionary representing the shop.
+* customer: A dictionary representing the customer.
+read_user_input()
+Reads user input in live mode for product name and quantity.
+operate_live_mode(shop)
+Operates the shop in live mode, processing orders based on real-time user input.
+* shop: A dictionary representing the shop.
--- oopshop.py
+++ oopshop.py
@@ -1,22 +1,26 @@
+# This imports a csv file.
+
 import csv
 
+# Product class representing each product whith its name, price and quantity.
 class Product:
     def __init__(self, name, price=0.0, quantity=0):
         self.name = name
         self.price = price
         self.quantity = quantity
-
+ # String representation of a product.
     def __str__(self):
         return f'NAME: {self.name}, PRICE: {self.price}, QUANTITY: {self.quantity}'
 
+# Shop class representing the shop.
 class Shop:
     def __init__(self, cash=0.0):
         self.cash = cash
         self.products = []
-
+# Adding product to the shop.
     def add_product(self, product):
         self.products.append(product)
-
+# Process a customer order.Checking if product is in stock and calculating total cost.
     def process_order(self, customer):
         total_cost = 0
         for c_product in customer.products:
@@ -28,6 +32,7 @@ class Shop:
                         print(f"Not enough stock for {c_product.name}")
                     break
 
+# Checking if customer has enough cash for the order. Updating shop inventory.
         if customer.cash >= total_cost:
             for c_product in customer.products:
                 for s_product in self.products:
@@ -40,36 +45,42 @@ class Shop:
         else:
             print(f"Customer {customer.name} does not have enough cash. Order not processed.")
 
+# String representation of the shop.
     def __str__(self):
         shop_str = "Shop Inventory\n"
         shop_str += f'Initial Cash: {self.cash}\n'
         shop_str += '\n'.join(str(product) for product in self.products)
         return shop_str
 
+ # Customer class representing each customer.
 class Customer:
     def __init__(self, name, cash=float('inf')):
         self.name = name
         self.cash = cash
         self.products = []
 
+# Adding a product to the order.
     def add_product(self, product):
         self.products.append(product)
 
+ # String representation of a customer.
     def __str__(self):
         customer_str = f'NAME: {self.name}, CASH: {self.cash}\n'
         customer_str += '\n'.join(f'NAME: {product.name}, QUANTITY: {product.quantity}' for product in self.products)
         return customer_str
 
+# Loading data from CSV files.
 def load_data(filename, is_shop=True):
     with open(filename) as file:
         csv_reader = csv.reader(file, delimiter=',')
         first_row = next(csv_reader)
 
+# Creating shop or customer. 
         if is_shop:
             entity = Shop(cash=float(first_row[0]))
         else:
             entity = Customer(name=first_row[0], cash=float(first_row[1]))
-
+# Adding product to the shop or customer.
         for row in csv_reader:
             name = row[0]
             if is_shop:
@@ -81,6 +92,7 @@ def load_data(filename, is_shop=True):
 
     return entity
 
+# Function that operates in live mode.
 def operate_live_mode(shop):
     while True:
         product_name = input("Enter product name: ")
@@ -93,7 +105,7 @@ def operate_live_mode(shop):
         print(live_customer)
         shop.process_order(live_customer)
 
-# Main execution
+# Loading data and processing orders for customers from CSV files
 shop = load_data('stock.csv', is_shop=True)
 print(shop)
 
@@ -104,6 +116,7 @@ for filename in customer_filenames:
     print(customer)
     shop.process_order(customer)
 
+# Prints text to screen
 print("\nEntering Live Mode:")
 print("\nWelcome to my live shop")
 print("\nI hope you enjoy your experience")
